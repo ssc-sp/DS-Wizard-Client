ds-wizard/engine-frontend

[DSW-883] Add UI for templates

engine-registry/elm/Registry/Pages/TemplateDetail.elm
@@ -1,0 +1,159 @@
+module Registry.Pages.TemplateDetail exposing
+    ( Model
+    , Msg
+    , init
+    , update
+    , view
+    )
+
+import ActionResult exposing (ActionResult(..))
+import Html exposing (Html, a, br, code, div, h5, li, p, strong, text, ul)
+import Html.Attributes exposing (class, href, target)
+import Markdown
+import Registry.Common.AppState exposing (AppState)
+import Registry.Common.Entities.OrganizationInfo exposing (OrganizationInfo)
+import Registry.Common.Entities.TemplateDetail exposing (TemplateDetail)
+import Registry.Common.Requests as Requests
+import Registry.Common.View.ItemIcon as ItemIcon
+import Registry.Common.View.Page as Page
+import Registry.Routing as Routing
+import Shared.Error.ApiError as ApiError exposing (ApiError)
+import Shared.Locale exposing (l, lx)
+import Version
+
+
+l_ : String -> AppState -> String
+l_ =
+    l "Registry.Pages.TemplateDetail"
+
+
+lx_ : String -> AppState -> Html msg
+lx_ =
+    lx "Registry.Pages.TemplateDetail"
+
+
+init : AppState -> String -> ( Model, Cmd Msg )
+init appState templateId =
+    ( { template = Loading }
+    , Requests.getTemplate appState templateId GetTemplateCompleted
+    )
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { template : ActionResult TemplateDetail }
+
+
+setTemplate : ActionResult TemplateDetail -> Model -> Model
+setTemplate template model =
+    { model | template = template }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = GetTemplateCompleted (Result ApiError TemplateDetail)
+
+
+update : Msg -> AppState -> Model -> Model
+update msg appState =
+    case msg of
+        GetTemplateCompleted result ->
+            ActionResult.apply setTemplate (ApiError.toActionResult (l_ "update.getError" appState)) result
+
+
+
+-- VIEW
+
+
+view : AppState -> Model -> Html Msg
+view appState model =
+    Page.actionResultView (viewDetail appState) model.template
+
+
+viewDetail : AppState -> TemplateDetail -> Html Msg
+viewDetail appState template =
+    let
+        viewKmId =
+            [ h5 [] [ lx_ "view.templateId" appState ]
+            , p []
+                [ code [] [ text template.id ] ]
+            ]
+
+        viewPublishedBy =
+            [ h5 [] [ lx_ "view.publishedBy" appState ]
+            , viewOrganization template.organization
+            ]
+
+        viewLicense =
+            [ h5 [] [ lx_ "view.license" appState ]
+            , p []
+                [ a [ href <| "https://spdx.org/licenses/" ++ template.license ++ ".html", target "_blank" ]
+                    [ text template.license ]
+                ]
+            ]
+
+        viewCurrentVersion =
+            [ h5 [] [ lx_ "view.version" appState ]
+            , p [] [ text <| Version.toString template.version ]
+            ]
+
+        otherVersions =
+            template.versions
+                |> List.filter ((/=) template.version)
+                |> List.sortWith Version.compare
+                |> List.reverse
+
+        viewOtherVersions =
+            case otherVersions of
+                [] ->
+                    []
+
+                versions ->
+                    [ h5 [] [ lx_ "view.otherVersions" appState ]
+                    , ul []
+                        (List.map viewVersion versions)
+                    ]
+
+        viewVersion version =
+            li []
+                [ a [ href <| Routing.toString <| Routing.KMDetail (template.organization.organizationId ++ ":" ++ template.templateId ++ ":" ++ Version.toString version) ]
+                    [ text <| Version.toString version ]
+                ]
+
+        viewSupportedMetamodel =
+            [ h5 [] [ lx_ "view.metamodelVersion" appState ]
+            , p [] [ text <| String.fromInt template.metamodelVersion ]
+            ]
+    in
+    div [ class "Detail" ]
+        [ div [ class "row" ]
+            [ div [ class "col-12 col-md-8" ]
+                [ Markdown.toHtml [] template.readme ]
+            , div [ class "Detail__Panel col-12 col-md-4" ]
+                (viewKmId
+                    ++ viewPublishedBy
+                    ++ viewLicense
+                    ++ viewCurrentVersion
+                    ++ viewOtherVersions
+                    ++ viewSupportedMetamodel
+                )
+            ]
+        ]
+
+
+viewOrganization : OrganizationInfo -> Html msg
+viewOrganization organization =
+    div [ class "organization" ]
+        [ ItemIcon.view { text = organization.name, image = organization.logo }
+        , div [ class "content" ]
+            [ strong [] [ text organization.name ]
+            , br [] []
+            , text organization.organizationId
+            ]
+        ]

engine-shared/elm/Shared/Api/Templates.elm
@@ -1,12 +1,20 @@
 module Shared.Api.Templates exposing
-    ( getTemplates
+    ( deleteTemplate
+    , deleteTemplateVersion
+    , exportTemplateUrl
+    , getTemplate
+    , getTemplates
     , getTemplatesFor
+    , importTemplate
+    , pullTemplate
     )
 
+import File exposing (File)
 import Json.Decode as D
 import Shared.AbstractAppState exposing (AbstractAppState)
-import Shared.Api exposing (ToMsg, jwtGet)
+import Shared.Api exposing (ToMsg, jwtDelete, jwtGet, jwtPostEmpty, jwtPostFile)
 import Shared.Data.Template as Template exposing (Template)
+import Shared.Data.TemplateDetail as TemplateDetail exposing (TemplateDetail)
 
 
 getTemplates : AbstractAppState a -> ToMsg (List Template) msg -> Cmd msg
@@ -14,6 +22,36 @@ getTemplates =
     jwtGet "/templates" (D.list Template.decoder)
 
 
+getTemplate : String -> AbstractAppState a -> ToMsg TemplateDetail msg -> Cmd msg
+getTemplate templateId =
+    jwtGet ("/templates/" ++ templateId) TemplateDetail.decoder
+
+
 getTemplatesFor : String -> AbstractAppState a -> ToMsg (List Template) msg -> Cmd msg
 getTemplatesFor pkgId =
     jwtGet ("/templates?pkgId=" ++ pkgId) (D.list Template.decoder)
+
+
+deleteTemplate : String -> String -> AbstractAppState a -> ToMsg () msg -> Cmd msg
+deleteTemplate organizationId templateId =
+    jwtDelete ("/templates/?organizationId=" ++ organizationId ++ "&kmId=" ++ templateId)
+
+
+deleteTemplateVersion : String -> AbstractAppState a -> ToMsg () msg -> Cmd msg
+deleteTemplateVersion templateId =
+    jwtDelete ("/templates/" ++ templateId)
+
+
+pullTemplate : String -> AbstractAppState a -> ToMsg () msg -> Cmd msg
+pullTemplate templateId =
+    jwtPostEmpty ("/templates/" ++ templateId ++ "/pull")
+
+
+importTemplate : File -> AbstractAppState a -> ToMsg () msg -> Cmd msg
+importTemplate =
+    jwtPostFile "/templates/bundle"
+
+
+exportTemplateUrl : String -> AbstractAppState a -> String
+exportTemplateUrl templateId appState =
+    appState.apiUrl ++ "/templates/" ++ templateId ++ "/bundle"

engine-wizard/elm/Wizard/Templates/Import/RegistryImport/View.elm
@@ -1,0 +1,106 @@
+module Wizard.Templates.Import.RegistryImport.View exposing (view)
+
+import ActionResult exposing (ActionResult(..))
+import Html exposing (Html, a, code, div, h1, hr, input, p, text)
+import Html.Attributes exposing (class, href, placeholder, target, type_, value)
+import Html.Events exposing (onInput)
+import Shared.Data.BootstrapConfig.RegistryConfig exposing (RegistryConfig(..))
+import Shared.Html exposing (emptyNode, faSet)
+import Shared.Locale exposing (l, lg, lh, lx)
+import Wizard.Common.AppState exposing (AppState)
+import Wizard.Common.Html exposing (linkTo)
+import Wizard.Common.View.ActionButton as ActionButton
+import Wizard.Common.View.FormResult as FormResult
+import Wizard.Routes as Routes
+import Wizard.Templates.Import.RegistryImport.Models exposing (Model)
+import Wizard.Templates.Import.RegistryImport.Msgs exposing (Msg(..))
+import Wizard.Templates.Routes
+
+
+l_ : String -> AppState -> String
+l_ =
+    l "Wizard.Templates.Import.RegistryImport.View"
+
+
+lh_ : String -> List (Html msg) -> AppState -> List (Html msg)
+lh_ =
+    lh "Wizard.Templates.Import.RegistryImport.View"
+
+
+lx_ : String -> AppState -> Html msg
+lx_ =
+    lx "Wizard.Templates.Import.RegistryImport.View"
+
+
+view : AppState -> Model -> Html Msg
+view appState model =
+    let
+        content =
+            case model.pulling of
+                Success _ ->
+                    viewImported appState model.templateId
+
+                _ ->
+                    viewForm appState model
+    in
+    div [ class "KnowledgeModels__Import__RegistryImport" ]
+        [ content ]
+
+
+viewForm : AppState -> Model -> Html Msg
+viewForm appState model =
+    div []
+        [ FormResult.errorOnlyView appState model.pulling
+        , div [ class "jumbotron" ]
+            [ div [ class "input-group" ]
+                [ input
+                    [ onInput ChangeTemplateId
+                    , type_ "text"
+                    , value model.templateId
+                    , class "form-control"
+                    , placeholder <| lg "template.id" appState
+                    ]
+                    []
+                , div [ class "input-group-append" ]
+                    [ ActionButton.button appState
+                        { label = l_ "form.import" appState
+                        , result = model.pulling
+                        , msg = Submit
+                        , dangerous = False
+                        }
+                    ]
+                ]
+            , hr [] []
+            , viewRegistryText appState
+            ]
+        ]
+
+
+viewRegistryText : AppState -> Html msg
+viewRegistryText appState =
+    case appState.config.registry of
+        RegistryEnabled url ->
+            p []
+                (lh_ "registryLink"
+                    [ a [ href url, target "_blank" ] [ lx_ "registry" appState ]
+                    ]
+                    appState
+                )
+
+        _ ->
+            emptyNode
+
+
+viewImported : AppState -> String -> Html Msg
+viewImported appState packageId =
+    div [ class "jumbotron" ]
+        [ h1 [] [ faSet "_global.success" appState ]
+        , p [ class "lead" ]
+            (lh_ "imported.message" [ code [] [ text packageId ] ] appState)
+        , p [ class "lead" ]
+            [ linkTo appState
+                (Routes.TemplatesRoute <| Wizard.Templates.Routes.DetailRoute packageId)
+                [ class "btn btn-primary" ]
+                [ lx_ "imported.action" appState ]
+            ]
+        ]

ssc-sp/DS-Wizard-Client

engine-registry/elm/Registry/Pages/TemplateDetail.elm
@@ -1,0 +1,159 @@
+module Registry.Pages.TemplateDetail exposing
+    ( Model
+    , Msg
+    , init
+    , update
+    , view
+    )
+
+import ActionResult exposing (ActionResult(..))
+import Html exposing (Html, a, br, code, div, h5, li, p, strong, text, ul)
+import Html.Attributes exposing (class, href, target)
+import Markdown
+import Registry.Common.AppState exposing (AppState)
+import Registry.Common.Entities.OrganizationInfo exposing (OrganizationInfo)
+import Registry.Common.Entities.TemplateDetail exposing (TemplateDetail)
+import Registry.Common.Requests as Requests
+import Registry.Common.View.ItemIcon as ItemIcon
+import Registry.Common.View.Page as Page
+import Registry.Routing as Routing
+import Shared.Error.ApiError as ApiError exposing (ApiError)
+import Shared.Locale exposing (l, lx)
+import Version
+
+
+l_ : String -> AppState -> String
+l_ =
+    l "Registry.Pages.TemplateDetail"
+
+
+lx_ : String -> AppState -> Html msg
+lx_ =
+    lx "Registry.Pages.TemplateDetail"
+
+
+init : AppState -> String -> ( Model, Cmd Msg )
+init appState templateId =
+    ( { template = Loading }
+    , Requests.getTemplate appState templateId GetTemplateCompleted
+    )
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { template : ActionResult TemplateDetail }
+
+
+setTemplate : ActionResult TemplateDetail -> Model -> Model
+setTemplate template model =
+    { model | template = template }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = GetTemplateCompleted (Result ApiError TemplateDetail)
+
+
+update : Msg -> AppState -> Model -> Model
+update msg appState =
+    case msg of
+        GetTemplateCompleted result ->
+            ActionResult.apply setTemplate (ApiError.toActionResult (l_ "update.getError" appState)) result
+
+
+
+-- VIEW
+
+
+view : AppState -> Model -> Html Msg
+view appState model =
+    Page.actionResultView (viewDetail appState) model.template
+
+
+viewDetail : AppState -> TemplateDetail -> Html Msg
+viewDetail appState template =
+    let
+        viewKmId =
+            [ h5 [] [ lx_ "view.templateId" appState ]
+            , p []
+                [ code [] [ text template.id ] ]
+            ]
+
+        viewPublishedBy =
+            [ h5 [] [ lx_ "view.publishedBy" appState ]
+            , viewOrganization template.organization
+            ]
+
+        viewLicense =
+            [ h5 [] [ lx_ "view.license" appState ]
+            , p []
+                [ a [ href <| "https://spdx.org/licenses/" ++ template.license ++ ".html", target "_blank" ]
+                    [ text template.license ]
+                ]
+            ]
+
+        viewCurrentVersion =
+            [ h5 [] [ lx_ "view.version" appState ]
+            , p [] [ text <| Version.toString template.version ]
+            ]
+
+        otherVersions =
+            template.versions
+                |> List.filter ((/=) template.version)
+                |> List.sortWith Version.compare
+                |> List.reverse
+
+        viewOtherVersions =
+            case otherVersions of
+                [] ->
+                    []
+
+                versions ->
+                    [ h5 [] [ lx_ "view.otherVersions" appState ]
+                    , ul []
+                        (List.map viewVersion versions)
+                    ]
+
+        viewVersion version =
+            li []
+                [ a [ href <| Routing.toString <| Routing.TemplateDetail (template.organization.organizationId ++ ":" ++ template.templateId ++ ":" ++ Version.toString version) ]
+                    [ text <| Version.toString version ]
+                ]
+
+        viewSupportedMetamodel =
+            [ h5 [] [ lx_ "view.metamodelVersion" appState ]
+            , p [] [ text <| String.fromInt template.metamodelVersion ]
+            ]
+    in
+    div [ class "Detail" ]
+        [ div [ class "row" ]
+            [ div [ class "col-12 col-md-8" ]
+                [ Markdown.toHtml [] template.readme ]
+            , div [ class "Detail__Panel col-12 col-md-4" ]
+                (viewKmId
+                    ++ viewPublishedBy
+                    ++ viewLicense
+                    ++ viewCurrentVersion
+                    ++ viewOtherVersions
+                    ++ viewSupportedMetamodel
+                )
+            ]
+        ]
+
+
+viewOrganization : OrganizationInfo -> Html msg
+viewOrganization organization =
+    div [ class "organization" ]
+        [ ItemIcon.view { text = organization.name, image = organization.logo }
+        , div [ class "content" ]
+            [ strong [] [ text organization.name ]
+            , br [] []
+            , text organization.organizationId
+            ]
+        ]

engine-shared/elm/Shared/Api/Templates.elm
@@ -1,12 +1,20 @@
 module Shared.Api.Templates exposing
-    ( getTemplates
+    ( deleteTemplate
+    , deleteTemplateVersion
+    , exportTemplateUrl
+    , getTemplate
+    , getTemplates
     , getTemplatesFor
+    , importTemplate
+    , pullTemplate
     )
 
+import File exposing (File)
 import Json.Decode as D
 import Shared.AbstractAppState exposing (AbstractAppState)
-import Shared.Api exposing (ToMsg, jwtGet)
+import Shared.Api exposing (ToMsg, jwtDelete, jwtGet, jwtPostEmpty, jwtPostFile)
 import Shared.Data.Template as Template exposing (Template)
+import Shared.Data.TemplateDetail as TemplateDetail exposing (TemplateDetail)
 
 
 getTemplates : AbstractAppState a -> ToMsg (List Template) msg -> Cmd msg
@@ -14,6 +22,36 @@ getTemplates =
     jwtGet "/templates" (D.list Template.decoder)
 
 
+getTemplate : String -> AbstractAppState a -> ToMsg TemplateDetail msg -> Cmd msg
+getTemplate templateId =
+    jwtGet ("/templates/" ++ templateId) TemplateDetail.decoder
+
+
 getTemplatesFor : String -> AbstractAppState a -> ToMsg (List Template) msg -> Cmd msg
 getTemplatesFor pkgId =
     jwtGet ("/templates?pkgId=" ++ pkgId) (D.list Template.decoder)
+
+
+deleteTemplate : String -> String -> AbstractAppState a -> ToMsg () msg -> Cmd msg
+deleteTemplate organizationId templateId =
+    jwtDelete ("/templates/?organizationId=" ++ organizationId ++ "&templateId=" ++ templateId)
+
+
+deleteTemplateVersion : String -> AbstractAppState a -> ToMsg () msg -> Cmd msg
+deleteTemplateVersion templateId =
+    jwtDelete ("/templates/" ++ templateId)
+
+
+pullTemplate : String -> AbstractAppState a -> ToMsg () msg -> Cmd msg
+pullTemplate templateId =
+    jwtPostEmpty ("/templates/" ++ templateId ++ "/pull")
+
+
+importTemplate : File -> AbstractAppState a -> ToMsg () msg -> Cmd msg
+importTemplate =
+    jwtPostFile "/templates/bundle"
+
+
+exportTemplateUrl : String -> AbstractAppState a -> String
+exportTemplateUrl templateId appState =
+    appState.apiUrl ++ "/templates/" ++ templateId ++ "/bundle"

engine-wizard/elm/Wizard/Templates/Import/RegistryImport/View.elm
@@ -1,0 +1,106 @@
+module Wizard.Templates.Import.RegistryImport.View exposing (view)
+
+import ActionResult exposing (ActionResult(..))
+import Html exposing (Html, a, code, div, h1, hr, input, p, text)
+import Html.Attributes exposing (class, href, placeholder, target, type_, value)
+import Html.Events exposing (onInput)
+import Shared.Data.BootstrapConfig.RegistryConfig exposing (RegistryConfig(..))
+import Shared.Html exposing (emptyNode, faSet)
+import Shared.Locale exposing (l, lg, lh, lx)
+import Wizard.Common.AppState exposing (AppState)
+import Wizard.Common.Html exposing (linkTo)
+import Wizard.Common.View.ActionButton as ActionButton
+import Wizard.Common.View.FormResult as FormResult
+import Wizard.Routes as Routes
+import Wizard.Templates.Import.RegistryImport.Models exposing (Model)
+import Wizard.Templates.Import.RegistryImport.Msgs exposing (Msg(..))
+import Wizard.Templates.Routes
+
+
+l_ : String -> AppState -> String
+l_ =
+    l "Wizard.Templates.Import.RegistryImport.View"
+
+
+lh_ : String -> List (Html msg) -> AppState -> List (Html msg)
+lh_ =
+    lh "Wizard.Templates.Import.RegistryImport.View"
+
+
+lx_ : String -> AppState -> Html msg
+lx_ =
+    lx "Wizard.Templates.Import.RegistryImport.View"
+
+
+view : AppState -> Model -> Html Msg
+view appState model =
+    let
+        content =
+            case model.pulling of
+                Success _ ->
+                    viewImported appState model.templateId
+
+                _ ->
+                    viewForm appState model
+    in
+    div [ class "KnowledgeModels__Import__RegistryImport" ]
+        [ content ]
+
+
+viewForm : AppState -> Model -> Html Msg
+viewForm appState model =
+    div []
+        [ FormResult.errorOnlyView appState model.pulling
+        , div [ class "jumbotron" ]
+            [ div [ class "input-group" ]
+                [ input
+                    [ onInput ChangeTemplateId
+                    , type_ "text"
+                    , value model.templateId
+                    , class "form-control"
+                    , placeholder <| lg "template.templateId" appState
+                    ]
+                    []
+                , div [ class "input-group-append" ]
+                    [ ActionButton.button appState
+                        { label = l_ "form.import" appState
+                        , result = model.pulling
+                        , msg = Submit
+                        , dangerous = False
+                        }
+                    ]
+                ]
+            , hr [] []
+            , viewRegistryText appState
+            ]
+        ]
+
+
+viewRegistryText : AppState -> Html msg
+viewRegistryText appState =
+    case appState.config.registry of
+        RegistryEnabled url ->
+            p []
+                (lh_ "registryLink"
+                    [ a [ href url, target "_blank" ] [ lx_ "registry" appState ]
+                    ]
+                    appState
+                )
+
+        _ ->
+            emptyNode
+
+
+viewImported : AppState -> String -> Html Msg
+viewImported appState packageId =
+    div [ class "jumbotron" ]
+        [ h1 [] [ faSet "_global.success" appState ]
+        , p [ class "lead" ]
+            (lh_ "imported.message" [ code [] [ text packageId ] ] appState)
+        , p [ class "lead" ]
+            [ linkTo appState
+                (Routes.TemplatesRoute <| Wizard.Templates.Routes.DetailRoute packageId)
+                [ class "btn btn-primary" ]
+                [ lx_ "imported.action" appState ]
+            ]
+        ]