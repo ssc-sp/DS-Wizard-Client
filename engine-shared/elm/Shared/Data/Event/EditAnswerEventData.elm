ds-wizard/engine-frontend

[DSW-1261] Add online collaboration for KM Editor

engine-shared/elm/Shared/Data/Event/EditAnswerEventData.elm
@@ -1,15 +1,18 @@
 module Shared.Data.Event.EditAnswerEventData exposing
     ( EditAnswerEventData
+    , apply
     , decoder
     , encode
+    , init
     )
 
-import Dict exposing (Dict)
 import Json.Decode as D exposing (Decoder)
 import Json.Decode.Pipeline as D
 import Json.Encode as E
 import Json.Encode.Extra as E
 import Shared.Data.Event.EventField as EventField exposing (EventField)
+import Shared.Data.KnowledgeModel.Annotation as Annotation exposing (Annotation)
+import Shared.Data.KnowledgeModel.Answer exposing (Answer)
 import Shared.Data.KnowledgeModel.MetricMeasure as MetricMeasure exposing (MetricMeasure)
 
 
@@ -18,7 +21,7 @@ type alias EditAnswerEventData =
     , advice : EventField (Maybe String)
     , metricMeasures : EventField (List MetricMeasure)
     , followUpUuids : EventField (List String)
-    , annotations : EventField (Dict String String)
+    , annotations : EventField (List Annotation)
     }
 
 
@@ -29,7 +32,7 @@ decoder =
         |> D.required "advice" (EventField.decoder (D.nullable D.string))
         |> D.required "metricMeasures" (EventField.decoder (D.list MetricMeasure.decoder))
         |> D.required "followUpUuids" (EventField.decoder (D.list D.string))
-        |> D.required "annotations" (EventField.decoder (D.dict D.string))
+        |> D.required "annotations" (EventField.decoder (D.list Annotation.decoder))
 
 
 encode : EditAnswerEventData -> List ( String, E.Value )
@@ -39,5 +42,26 @@ encode data =
     , ( "advice", EventField.encode (E.maybe E.string) data.advice )
     , ( "metricMeasures", EventField.encode (E.list MetricMeasure.encode) data.metricMeasures )
     , ( "followUpUuids", EventField.encode (E.list E.string) data.followUpUuids )
-    , ( "annotations", EventField.encode (E.dict identity E.string) data.annotations )
+    , ( "annotations", EventField.encode (E.list Annotation.encode) data.annotations )
     ]
+
+
+init : EditAnswerEventData
+init =
+    { label = EventField.empty
+    , advice = EventField.empty
+    , metricMeasures = EventField.empty
+    , followUpUuids = EventField.empty
+    , annotations = EventField.empty
+    }
+
+
+apply : EditAnswerEventData -> Answer -> Answer
+apply eventData answer =
+    { answer
+        | label = EventField.getValueWithDefault eventData.label answer.label
+        , advice = EventField.getValueWithDefault eventData.advice answer.advice
+        , metricMeasures = EventField.getValueWithDefault eventData.metricMeasures answer.metricMeasures
+        , followUpUuids = EventField.getValueWithDefault eventData.followUpUuids answer.followUpUuids
+        , annotations = EventField.getValueWithDefault eventData.annotations answer.annotations
+    }

engine-shared/elm/Shared/Data/Event/EditChapterEventData.elm
@@ -1,22 +1,25 @@
 module Shared.Data.Event.EditChapterEventData exposing
     ( EditChapterEventData
+    , apply
     , decoder
     , encode
+    , init
     )
 
-import Dict exposing (Dict)
 import Json.Decode as D exposing (Decoder)
 import Json.Decode.Pipeline as D
 import Json.Encode as E
 import Json.Encode.Extra as E
 import Shared.Data.Event.EventField as EventField exposing (EventField)
+import Shared.Data.KnowledgeModel.Annotation as Annotation exposing (Annotation)
+import Shared.Data.KnowledgeModel.Chapter exposing (Chapter)
 
 
 type alias EditChapterEventData =
     { title : EventField String
     , text : EventField (Maybe String)
     , questionUuids : EventField (List String)
-    , annotations : EventField (Dict String String)
+    , annotations : EventField (List Annotation)
     }
 
 
@@ -26,7 +29,7 @@ decoder =
         |> D.required "title" (EventField.decoder D.string)
         |> D.required "text" (EventField.decoder (D.nullable D.string))
         |> D.required "questionUuids" (EventField.decoder (D.list D.string))
-        |> D.required "annotations" (EventField.decoder (D.dict D.string))
+        |> D.required "annotations" (EventField.decoder (D.list Annotation.decoder))
 
 
 encode : EditChapterEventData -> List ( String, E.Value )
@@ -35,5 +38,24 @@ encode data =
     , ( "title", EventField.encode E.string data.title )
     , ( "text", EventField.encode (E.maybe E.string) data.text )
     , ( "questionUuids", EventField.encode (E.list E.string) data.questionUuids )
-    , ( "annotations", EventField.encode (E.dict identity E.string) data.annotations )
+    , ( "annotations", EventField.encode (E.list Annotation.encode) data.annotations )
     ]
+
+
+init : EditChapterEventData
+init =
+    { title = EventField.empty
+    , text = EventField.empty
+    , questionUuids = EventField.empty
+    , annotations = EventField.empty
+    }
+
+
+apply : EditChapterEventData -> Chapter -> Chapter
+apply eventData chapter =
+    { chapter
+        | title = EventField.getValueWithDefault eventData.title chapter.title
+        , text = EventField.getValueWithDefault eventData.text chapter.text
+        , questionUuids = EventField.getValueWithDefault eventData.questionUuids chapter.questionUuids
+        , annotations = EventField.getValueWithDefault eventData.annotations chapter.annotations
+    }

engine-shared/elm/Shared/Data/Event/EditQuestionEventData.elm
@@ -1,5 +1,6 @@
 module Shared.Data.Event.EditQuestionEventData exposing
     ( EditQuestionEventData(..)
+    , apply
     , decoder
     , encode
     , getEntityVisibleName
@@ -7,6 +8,7 @@ module Shared.Data.Event.EditQuestionEventData exposing
     , map
     )
 
+import Dict
 import Json.Decode as D exposing (Decoder)
 import Json.Encode as E
 import Shared.Data.Event.EditQuestionIntegrationEventData as EditQuestionIntegrationEventData exposing (EditQuestionIntegrationEventData)
@@ -15,6 +17,8 @@ import Shared.Data.Event.EditQuestionMultiChoiceEventData as EditQuestionMultiCh
 import Shared.Data.Event.EditQuestionOptionsEventData as EditQuestionOptionsEventData exposing (EditQuestionOptionsEventData)
 import Shared.Data.Event.EditQuestionValueEventData as EditQuestionValueEventData exposing (EditQuestionValueEventData)
 import Shared.Data.Event.EventField as EventField
+import Shared.Data.KnowledgeModel.Question as Question exposing (Question(..))
+import Shared.Data.KnowledgeModel.Question.QuestionValueType as QuestionValueType exposing (QuestionValueType(..))
 
 
 type EditQuestionEventData
@@ -64,6 +68,53 @@ encode data =
                 data
     in
     ( "eventType", E.string "EditQuestionEvent" ) :: eventData
+
+
+apply : EditQuestionEventData -> Question -> Question
+apply event question =
+    let
+        applyCommonData data =
+            { uuid = Question.getUuid question
+            , title = EventField.getValueWithDefault data.title (Question.getTitle question)
+            , text = EventField.getValueWithDefault data.text (Question.getText question)
+            , requiredPhaseUuid = EventField.getValueWithDefault data.requiredPhaseUuid (Question.getRequiredPhaseUuid question)
+            , tagUuids = EventField.getValueWithDefault data.tagUuids (Question.getTagUuids question)
+            , referenceUuids = EventField.getValueWithDefault data.referenceUuids (Question.getReferenceUuids question)
+            , expertUuids = EventField.getValueWithDefault data.expertUuids (Question.getExpertUuids question)
+            , annotations = EventField.getValueWithDefault data.annotations (Question.getAnnotations question)
+            }
+    in
+    case event of
+        EditQuestionOptionsEvent eventData ->
+            OptionsQuestion
+                (applyCommonData eventData)
+                { answerUuids = EventField.getValueWithDefault eventData.answerUuids (Question.getAnswerUuids question)
+                }
+
+        EditQuestionListEvent eventData ->
+            ListQuestion
+                (applyCommonData eventData)
+                { itemTemplateQuestionUuids = EventField.getValueWithDefault eventData.itemTemplateQuestionUuids (Question.getItemTemplateQuestionUuids question)
+                }
+
+        EditQuestionValueEvent eventData ->
+            ValueQuestion
+                (applyCommonData eventData)
+                { valueType = EventField.getValueWithDefault eventData.valueType (Maybe.withDefault QuestionValueType.default (Question.getValueType question))
+                }
+
+        EditQuestionIntegrationEvent eventData ->
+            IntegrationQuestion
+                (applyCommonData eventData)
+                { integrationUuid = EventField.getValueWithDefault eventData.integrationUuid (Maybe.withDefault "" (Question.getIntegrationUuid question))
+                , props = EventField.getValueWithDefault eventData.props (Maybe.withDefault Dict.empty (Question.getProps question))
+                }
+
+        EditQuestionMultiChoiceEvent eventData ->
+            MultiChoiceQuestion
+                (applyCommonData eventData)
+                { choiceUuids = EventField.getValueWithDefault eventData.choiceUuids (Question.getChoiceUuids question)
+                }
 
 
 getTypeString : EditQuestionEventData -> String

ssc-sp/DS-Wizard-Client

engine-shared/elm/Shared/Data/Event/EditAnswerEventData.elm
@@ -1,15 +1,18 @@
 module Shared.Data.Event.EditAnswerEventData exposing
     ( EditAnswerEventData
+    , apply
     , decoder
     , encode
+    , init
     )
 
-import Dict exposing (Dict)
 import Json.Decode as D exposing (Decoder)
 import Json.Decode.Pipeline as D
 import Json.Encode as E
 import Json.Encode.Extra as E
 import Shared.Data.Event.EventField as EventField exposing (EventField)
+import Shared.Data.KnowledgeModel.Annotation as Annotation exposing (Annotation)
+import Shared.Data.KnowledgeModel.Answer exposing (Answer)
 import Shared.Data.KnowledgeModel.MetricMeasure as MetricMeasure exposing (MetricMeasure)
 
 
@@ -18,7 +21,7 @@ type alias EditAnswerEventData =
     , advice : EventField (Maybe String)
     , metricMeasures : EventField (List MetricMeasure)
     , followUpUuids : EventField (List String)
-    , annotations : EventField (Dict String String)
+    , annotations : EventField (List Annotation)
     }
 
 
@@ -29,7 +32,7 @@ decoder =
         |> D.required "advice" (EventField.decoder (D.nullable D.string))
         |> D.required "metricMeasures" (EventField.decoder (D.list MetricMeasure.decoder))
         |> D.required "followUpUuids" (EventField.decoder (D.list D.string))
-        |> D.required "annotations" (EventField.decoder (D.dict D.string))
+        |> D.required "annotations" (EventField.decoder (D.list Annotation.decoder))
 
 
 encode : EditAnswerEventData -> List ( String, E.Value )
@@ -39,5 +42,26 @@ encode data =
     , ( "advice", EventField.encode (E.maybe E.string) data.advice )
     , ( "metricMeasures", EventField.encode (E.list MetricMeasure.encode) data.metricMeasures )
     , ( "followUpUuids", EventField.encode (E.list E.string) data.followUpUuids )
-    , ( "annotations", EventField.encode (E.dict identity E.string) data.annotations )
+    , ( "annotations", EventField.encode (E.list Annotation.encode) data.annotations )
     ]
+
+
+init : EditAnswerEventData
+init =
+    { label = EventField.empty
+    , advice = EventField.empty
+    , metricMeasures = EventField.empty
+    , followUpUuids = EventField.empty
+    , annotations = EventField.empty
+    }
+
+
+apply : EditAnswerEventData -> Answer -> Answer
+apply eventData answer =
+    { answer
+        | label = EventField.getValueWithDefault eventData.label answer.label
+        , advice = EventField.getValueWithDefault eventData.advice answer.advice
+        , metricMeasures = EventField.getValueWithDefault eventData.metricMeasures answer.metricMeasures
+        , followUpUuids = EventField.applyChildren eventData.followUpUuids answer.followUpUuids
+        , annotations = EventField.getValueWithDefault eventData.annotations answer.annotations
+    }

engine-shared/elm/Shared/Data/Event/EditChapterEventData.elm
@@ -1,22 +1,25 @@
 module Shared.Data.Event.EditChapterEventData exposing
     ( EditChapterEventData
+    , apply
     , decoder
     , encode
+    , init
     )
 
-import Dict exposing (Dict)
 import Json.Decode as D exposing (Decoder)
 import Json.Decode.Pipeline as D
 import Json.Encode as E
 import Json.Encode.Extra as E
 import Shared.Data.Event.EventField as EventField exposing (EventField)
+import Shared.Data.KnowledgeModel.Annotation as Annotation exposing (Annotation)
+import Shared.Data.KnowledgeModel.Chapter exposing (Chapter)
 
 
 type alias EditChapterEventData =
     { title : EventField String
     , text : EventField (Maybe String)
     , questionUuids : EventField (List String)
-    , annotations : EventField (Dict String String)
+    , annotations : EventField (List Annotation)
     }
 
 
@@ -26,7 +29,7 @@ decoder =
         |> D.required "title" (EventField.decoder D.string)
         |> D.required "text" (EventField.decoder (D.nullable D.string))
         |> D.required "questionUuids" (EventField.decoder (D.list D.string))
-        |> D.required "annotations" (EventField.decoder (D.dict D.string))
+        |> D.required "annotations" (EventField.decoder (D.list Annotation.decoder))
 
 
 encode : EditChapterEventData -> List ( String, E.Value )
@@ -35,5 +38,24 @@ encode data =
     , ( "title", EventField.encode E.string data.title )
     , ( "text", EventField.encode (E.maybe E.string) data.text )
     , ( "questionUuids", EventField.encode (E.list E.string) data.questionUuids )
-    , ( "annotations", EventField.encode (E.dict identity E.string) data.annotations )
+    , ( "annotations", EventField.encode (E.list Annotation.encode) data.annotations )
     ]
+
+
+init : EditChapterEventData
+init =
+    { title = EventField.empty
+    , text = EventField.empty
+    , questionUuids = EventField.empty
+    , annotations = EventField.empty
+    }
+
+
+apply : EditChapterEventData -> Chapter -> Chapter
+apply eventData chapter =
+    { chapter
+        | title = EventField.getValueWithDefault eventData.title chapter.title
+        , text = EventField.getValueWithDefault eventData.text chapter.text
+        , questionUuids = EventField.applyChildren eventData.questionUuids chapter.questionUuids
+        , annotations = EventField.getValueWithDefault eventData.annotations chapter.annotations
+    }

engine-shared/elm/Shared/Data/Event/EditQuestionEventData.elm
@@ -1,5 +1,6 @@
 module Shared.Data.Event.EditQuestionEventData exposing
     ( EditQuestionEventData(..)
+    , apply
     , decoder
     , encode
     , getEntityVisibleName
@@ -7,6 +8,7 @@ module Shared.Data.Event.EditQuestionEventData exposing
     , map
     )
 
+import Dict
 import Json.Decode as D exposing (Decoder)
 import Json.Encode as E
 import Shared.Data.Event.EditQuestionIntegrationEventData as EditQuestionIntegrationEventData exposing (EditQuestionIntegrationEventData)
@@ -15,6 +17,8 @@ import Shared.Data.Event.EditQuestionMultiChoiceEventData as EditQuestionMultiCh
 import Shared.Data.Event.EditQuestionOptionsEventData as EditQuestionOptionsEventData exposing (EditQuestionOptionsEventData)
 import Shared.Data.Event.EditQuestionValueEventData as EditQuestionValueEventData exposing (EditQuestionValueEventData)
 import Shared.Data.Event.EventField as EventField
+import Shared.Data.KnowledgeModel.Question as Question exposing (Question(..))
+import Shared.Data.KnowledgeModel.Question.QuestionValueType as QuestionValueType exposing (QuestionValueType(..))
 
 
 type EditQuestionEventData
@@ -64,6 +68,53 @@ encode data =
                 data
     in
     ( "eventType", E.string "EditQuestionEvent" ) :: eventData
+
+
+apply : EditQuestionEventData -> Question -> Question
+apply event question =
+    let
+        applyCommonData data =
+            { uuid = Question.getUuid question
+            , title = EventField.getValueWithDefault data.title (Question.getTitle question)
+            , text = EventField.getValueWithDefault data.text (Question.getText question)
+            , requiredPhaseUuid = EventField.getValueWithDefault data.requiredPhaseUuid (Question.getRequiredPhaseUuid question)
+            , tagUuids = EventField.getValueWithDefault data.tagUuids (Question.getTagUuids question)
+            , referenceUuids = EventField.applyChildren data.referenceUuids (Question.getReferenceUuids question)
+            , expertUuids = EventField.applyChildren data.expertUuids (Question.getExpertUuids question)
+            , annotations = EventField.getValueWithDefault data.annotations (Question.getAnnotations question)
+            }
+    in
+    case event of
+        EditQuestionOptionsEvent eventData ->
+            OptionsQuestion
+                (applyCommonData eventData)
+                { answerUuids = EventField.applyChildren eventData.answerUuids (Question.getAnswerUuids question)
+                }
+
+        EditQuestionListEvent eventData ->
+            ListQuestion
+                (applyCommonData eventData)
+                { itemTemplateQuestionUuids = EventField.applyChildren eventData.itemTemplateQuestionUuids (Question.getItemTemplateQuestionUuids question)
+                }
+
+        EditQuestionValueEvent eventData ->
+            ValueQuestion
+                (applyCommonData eventData)
+                { valueType = EventField.getValueWithDefault eventData.valueType (Maybe.withDefault QuestionValueType.default (Question.getValueType question))
+                }
+
+        EditQuestionIntegrationEvent eventData ->
+            IntegrationQuestion
+                (applyCommonData eventData)
+                { integrationUuid = EventField.getValueWithDefault eventData.integrationUuid (Maybe.withDefault "" (Question.getIntegrationUuid question))
+                , props = EventField.getValueWithDefault eventData.props (Maybe.withDefault Dict.empty (Question.getProps question))
+                }
+
+        EditQuestionMultiChoiceEvent eventData ->
+            MultiChoiceQuestion
+                (applyCommonData eventData)
+                { choiceUuids = EventField.applyChildren eventData.choiceUuids (Question.getChoiceUuids question)
+                }
 
 
 getTypeString : EditQuestionEventData -> String