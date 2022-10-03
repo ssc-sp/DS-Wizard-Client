4087e3052c4ee1a4c64ec2d12f69280eda8fbd63
root <root@DESKTOP-K6CKA6N.localdomain>
2022-10-03 18:21:07 -0400

    removed

--- scripts/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-read -p "Copy UNIX path to DS-Wizard-Client folder on your host system: " wpath
-rm -r $HOME/DS-Wizard-Client/engine-wizard
-rm -r $HOME/DS-Wizard-Client/dsw-deployment-example/assets
-ln -s $wpath/engine-wizard $HOME/DS-Wizard-Client/engine-wizard
-ln -s $wpath/dsw-deployment-example/assets $HOME/DS-Wizard-Client/dsw-deployment-example/assets
